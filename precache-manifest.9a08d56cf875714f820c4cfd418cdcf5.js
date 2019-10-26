7b87498f9a067dae7d312c6a389132e5860d401e
Serhii <gespenst.serge@yandex.ru>
2019-10-26 23:16:11 +0300

    Updates

---
+++ precache-manifest.9a08d56cf875714f820c4cfd418cdcf5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e27e9e680314ad936ebab053af622899",
+    "url": "/react_static-list-of-todos/index.html"
+  },
+  {
+    "revision": "2ba631f5e39c79987060",
+    "url": "/react_static-list-of-todos/static/css/main.6b0b4046.chunk.css"
+  },
+  {
+    "revision": "3668340bf9fc04d4ba23",
+    "url": "/react_static-list-of-todos/static/js/2.0206139a.chunk.js"
+  },
+  {
+    "revision": "2ba631f5e39c79987060",
+    "url": "/react_static-list-of-todos/static/js/main.c17a1c38.chunk.js"
+  },
+  {
+    "revision": "ea6f31786c2aed313835",
+    "url": "/react_static-list-of-todos/static/js/runtime~main.00529491.js"
+  }
+]);
